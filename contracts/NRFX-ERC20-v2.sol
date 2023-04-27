e8938a8964a5f12c5f6f129d42537d699dae981f
Danil Sakhinov <ldevil2209@ya.ru>
2023-04-27 12:33:43 +0800

    Add ability to toggle migration availability. Add compability with two version of tokens

--- contracts/NRFX-ERC20-v2.sol
+++ contracts/NRFX-ERC20-v2.sol
@@ -11,30 +11,53 @@ contract NarfexToken is ERC20, Ownable {
     event Migrate(address indexed account, uint256 amount);
     event MasterChefSet(address newAddress);
     event BurnPercentageSet(uint256 percents);
+    event MigrationStop();
+    event MigrationRun();
 
     uint256 constant PERCENT_PRECISION = 10**4; // 10000 = 100%
     uint256 constant AUTOBURN_LIMIT = 10**7 wei; // TotalSupply limit above which burning works
     uint256 private _burnPercentage = 30; // 0.3% of burn/farming fee
-    address immutable public oldToken; // Old NRFX address for migration
+    address immutable public v1Token; // Old NRFX address for migration v1
+    address immutable public v2Token; // Old NRFX address for migration v2
     address public masterChef; // Farming contract address
+    bool public isMigrationEnabled = true; // Is allowed to migrate
 
-    /// @param oldToken_ Olf NRFX address
+    /// @param v1Token_ Olf NRFX address
+    /// @param v2Token_ Olf NRFX address
     /// @param masterChef_ Farming contract address
-    constructor(address oldToken_, address masterChef_) ERC20('Narfex', 'NRFX') {
-        oldToken = oldToken_;
+    constructor(address v1Token_, address v2Token_, address masterChef_) ERC20('Narfex', 'NRFX') {
+        v1Token = v1Token_;
+        v2Token = v2Token_;
         masterChef = masterChef_;
+
+        runMigration();
+        emit MasterChefSet(masterChef_);
     }
 
     /// @notice Migration from an old token to a new one
+    /// @param isV2 True if selected old token version is 2
     /// @dev Withdraw all account funds from the old token and mint on the new one
-    function migrate() public {
-        uint256 balance = IERC20(oldToken).balanceOf(msg.sender);
+    function migrate(bool isV2) public {
+        require(isMigrationEnabled, "Migration is not enabled");
+
+        // Select old token version
+        address token = isV2 ? v2Token : v1Token;
+        require(token != address(0), "Old token address not set");
+
+        // Check token balance
+        uint256 balance = IERC20(token).balanceOf(msg.sender);
         require(balance > 0, "Insufficient balance");
 
-        IERC20(oldToken).transferFrom(msg.sender, address(this), balance);
+        // Swap tokens
+        IERC20(token).transferFrom(msg.sender, address(this), balance);
         _mint(msg.sender, balance);
         
         emit Migrate(msg.sender, balance);
+    }
+
+    /// @notice Shortcut function for compability
+    function migrate() public {
+        migrate(false);
     }
 
     /// @notice Transfer with burning or sending to a farming contract
@@ -49,6 +72,7 @@ contract NarfexToken is ERC20, Ownable {
         uint256 transferAmount = amount - fee;
         _transfer(spender, receiver, transferAmount);
 
+        if (fee == 0) return;
         // Burn fee or transfer fee to a farming contract
         if (totalSupply() >= AUTOBURN_LIMIT) {
             _burn(spender, fee);
@@ -90,5 +114,19 @@ contract NarfexToken is ERC20, Ownable {
         require(percents < 10000, "Fee can't be higher than 100%");
         _burnPercentage = percents;
         emit BurnPercentageSet(percents);
+    }
+
+    /// @notice Allow to migrate tokens
+    function runMigration() public onlyOwner {
+        require(!isMigrationEnabled, "Migration already runned");
+        isMigrationEnabled = true;
+        emit MigrationRun();
+    }
+
+    /// @notice Prevent account from migration
+    function stopMigration() public onlyOwner {
+        require(isMigrationEnabled, "Migration already stopped");
+        isMigrationEnabled = false;
+        emit MigrationStop();
     }
 }
